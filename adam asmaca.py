c6fec65122078e32829189c0679fc9ab9bb83d33
ofckinq <94473750+ofckinq@users.noreply.github.com>
2021-12-18 22:12:24 +0300

    Add files via upload

---
+++ adam asmaca.py
@@ -0,0 +1,95 @@
+import random
+def adamas():
+
+    kelimeler = random.choice(["terminator" , "avengers" , "matrix" , "tenet" , "jumanji" , "shazam" , "lucy" , "deadpool" , "kingsman" , "transformers" ])
+    gecerliHarfler = 'abcdefghijklmnopqrstuvwxyz'
+    toplamHak = 10
+    yapilanTahmin = ''
+
+    while len(kelimeler) > 0:
+        asilKelime = ""
+        hataliTahminSayisi = 0
+
+        for harf in kelimeler:
+            if harf in yapilanTahmin:
+                asilKelime = asilKelime + harf
+            else:
+                asilKelime = asilKelime + "_" + " "
+        if asilKelime == kelimeler:
+            print(asilKelime)
+            print("Tebrikler kazandınız")
+            break
+
+        print("Filmin adını tahmin edin:" , asilKelime)
+        tahmin = input()
+
+        if tahmin in gecerliHarfler:
+            yapilanTahmin = yapilanTahmin + tahmin
+        else:
+            print("girdiğiniz harfi kontrol edin")
+            tahmin = input()
+
+        if tahmin not in kelimeler:
+            toplamHak = toplamHak - 1
+            if toplamHak == 9:
+                print("9 hakkınız kaldı")
+                print("  --------  ")
+            if toplamHak == 8:
+                print("8 hakkınız kaldı")
+                print("  --------  ")
+                print("     O      ")
+            if toplamHak == 7:
+                print("7 hakkınız kaldı")
+                print("  --------  ")
+                print("     O      ")
+                print("     |      ")
+            if toplamHak == 6:
+                print("6 hakkınız kaldı")
+                print("  --------  ")
+                print("     O      ")
+                print("     |      ")
+                print("    /       ")
+            if toplamHak == 5:
+                print("5 hakkınız kaldı")
+                print("  --------  ")
+                print("     O      ")
+                print("     |      ")
+                print("    / \     ")
+            if toplamHak == 4:
+                print("4 hakkınız kaldı")
+                print("  --------  ")
+                print("   \ O      ")
+                print("     |      ")
+                print("    / \     ")
+            if toplamHak == 3:
+                print("3 hakkınız kaldı")
+                print("  --------  ")
+                print("   \ O /    ")
+                print("     |      ")
+                print("    / \     ")
+            if toplamHak == 2:
+                print("2 hakkınız kaldı")
+                print("  --------  ")
+                print("   \ O /|   ")
+                print("     |      ")
+                print("    / \     ")
+            if toplamHak == 1:
+                print("1 hakkınız kaldı")
+                print("Dikkatli kullanın")
+                print("  --------  ")
+                print("   \ O_|/   ")
+                print("     |      ")
+                print("    / \     ")
+            if toplamHak == 0:
+                print("Oyun bitti")
+                print("bilemediniz ve kaybettiniz")
+                print("  --------  ")
+                print("     O_|    ")
+                print("    /|\      ")
+                print("    / \     ")
+                break
+
+
+print("Filmin adını bulabilmek için 10 hakkınız var")
+adamas()
+print()
